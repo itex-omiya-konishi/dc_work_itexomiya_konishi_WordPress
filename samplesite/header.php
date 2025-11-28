d76c6e34031c260bf9247059f0ecd9b4faea3be2
itex-omiya-konishi <it.omiya237@gmail.com>
2025-11-28 15:58:05 +0900

    02

---
+++ samplesite/header.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="container">
+        <h1><a href="index.html"><img src="images/logo.png" alt="SAMPLE WEB SITE" /></a></h1>
+        <div id="menu">
+            <ul>
+                <li class="home"><a href="index.html">TOP</a></li>
+                <li><a href="company.html">会社概要</a></li>
+                <li><a href="service.html">サービス概要</a></li>
+                <li><a href="archive-news.html">プレスリリース</a></li>
+                <li><a href="archive-news.html">ブログ</a></li>
+                <li><a href="contact.html">お問い合わせ</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
